d31bb5bdace52fee566c8a2dcfa229f633775d04
WYP0 <62331592+WYP0@users.noreply.github.com>
2022-04-14 21:54:08 +0800

    Add files via upload
    voice control

---
+++ const.py
@@ -0,0 +1,11 @@
+#在该文件中设定百度语音识别的参数
+APPID = 25713554
+
+APPKEY = "X72Nc5j9m08uVUKGebMcEq0G"
+
+# 语言模型 ， 可以修改为其它语言模型测试，如远场普通话19362
+DEV_PID = 15372
+
+# 可以改为wss://
+URI = "ws://vop.baidu.com/realtime_asr"
+
