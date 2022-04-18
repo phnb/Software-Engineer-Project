8493b5ee5776734e2a98b90f8da6f596f120afaa
WYP0 <119010322@link.cuhk.edu.cn>
2022-04-18 22:57:34 +0800

    settings

--- cashsys/cashsys/settings.py
+++ cashsys/cashsys/settings.py
@@ -96,23 +96,23 @@ WSGI_APPLICATION = 'cashsys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'monager',
-#         'USER': 'monager',
-#         'PASSWORD': '123456',
-#         'HOST': 'localhost',  
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'monager',
+        'USER': 'monager',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',  
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
